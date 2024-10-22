296a62ff4a5c421ff2bc731f9438c6d87f922d65
TuanOnta <mdz54dzakiarta@gmail.com>
2024-10-22 20:19:53 +0700

    feat: add hill climbing steepest ascent algorithm

--- backend/algorithms/hill_climbing_steepest.go
+++ backend/algorithms/hill_climbing_steepest.go
@@ -1,6 +1,39 @@
 package algorithms
 
-func HillClimbingSteepest() string {
-    // Example pseudo-algorithm logic
-    return "Steepest Accent Hill Climbing completed."
+import (
+	"fmt"
+)
+
+// Fungsi Hill Climbing Steepest Ascent
+func HillClimbingSteepest(initialState Cube) Solution {
+	currentCube := initialState
+	currentScore := currentCube.GetCurrentScore()
+
+	res := NewSolution()
+	res.AddSolutionItem(0, currentScore, currentCube.currentState)
+
+	i := 1
+
+	// Iterasi sampai mencapai kondisi optimal atau maksimum iterasi
+	for {
+		bestSuccessor := currentCube.GetBestSuccessor()
+		bestSuccessorScore := bestSuccessor.GetCurrentScore()
+
+		// If the best successor is better than the current cube, move to the new state
+		if bestSuccessorScore < currentScore {
+			currentCube = *bestSuccessor.CopyCube()
+			currentScore = bestSuccessorScore
+			res.AddSolutionItem(i, currentScore, currentCube.currentState)
+
+			i++
+
+			// Output the current score (optional, for tracking progress)
+			fmt.Println("Current score:", currentScore)
+		} else {
+			// If no better successor is found, stop the algorithm
+			break
+		}
+	}
+
+	return *res
 }
