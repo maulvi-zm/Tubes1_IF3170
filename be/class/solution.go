35c4b4acce5479d31bb1f0b17a68556e201df660
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-10-22 11:06:50 +0700

    feat: solution class

---
+++ be/class/solution.go
@@ -0,0 +1,23 @@
+package class
+
+type SolutionItem struct {
+	iteration int
+	score     int
+	state    []int
+}
+
+type Solution struct {
+	solution []SolutionItem
+}
+
+func NewSolution() *Solution {
+	return &Solution{solution: make([]SolutionItem, 0)}
+}
+
+func (s *Solution) AddSolutionItem(iteration int, score int, state []int) {
+	s.solution = append(s.solution, SolutionItem{iteration: iteration, score: score, state: state})
+}
+
+func (s *Solution) GetSolution() []SolutionItem {
+	return s.solution
+}
