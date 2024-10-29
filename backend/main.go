b1a0a719832ca963579c363cea1e4fc1792f1575
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-10-29 17:19:03 +0700

    fix: route

--- backend/main.go
+++ backend/main.go
@@ -9,18 +9,20 @@ import (
 )
 
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    // Endpoints for algorithms
-    r.HandleFunc("/hill-climbing/steepest", handlers.HillClimbingSteepestHandler).Methods("GET")
-    r.HandleFunc("/hill-climbing/stochastic", handlers.HillClimbingStochasticHandler).Methods("GET")
-    r.HandleFunc("/genetic-algorithm", handlers.GeneticAlgorithmHandler).Methods("GET")
-    r.HandleFunc("/simulated-annealing", handlers.SimulatedAnnealingHandler).Methods("GET")
+	// Endpoints for algorithms
+	r.HandleFunc("/hill-climbing/steepest", handlers.HillClimbingSteepestHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/stochastic", handlers.HillClimbingStochasticHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/sidewaymove", handlers.HillClimbingSidewayMoveHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/randomrestart", handlers.HillClimbingRandomRestartHandler).Methods("POST")
+	r.HandleFunc("/genetic-algorithm", handlers.GeneticAlgorithmHandler).Methods("POST")
+	r.HandleFunc("/simulated-annealing", handlers.SimulatedAnnealingHandler).Methods("POST")
 
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Welcome to the Algorithms API!"))
-    })
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Algorithms API!"))
+	})
 
-    log.Println("Server running on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
