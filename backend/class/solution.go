6072cc823596819623acc2d0beaad39c55ae42c6
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-10-31 19:21:06 +0700

    feat: add additional information for solution class

--- backend/class/solution.go
+++ backend/class/solution.go
@@ -8,18 +8,38 @@ type SolutionItem struct {
 	State     []int `json:"state"`
 	Iteration int   `json:"iteration"`
 	Score     int   `json:"score"`
+	DeltaE    int   `json:"deltaE"`
+}
+
+type addtitionalInfor struct {
+	ItemName  string `json:"itemName"`
+	ItemValue string `json:"itemValue"`
 }
 
 type Solution struct {
-	Solution []SolutionItem `json:"solutions"`
+	Type           string             `json:"type"`
+	Solution       []SolutionItem     `json:"solutions"`
+	AdditionalInfo []addtitionalInfor `json:"additionalInfo"`
 }
 
 func NewSolution() *Solution {
-	return &Solution{Solution: make([]SolutionItem, 0)}
+	return &Solution{Solution: make([]SolutionItem, 0), Type: "default", AdditionalInfo: make([]addtitionalInfor, 0)}
 }
 
-func (s *Solution) AddSolutionItem(iteration int, score int, state []int) {
-	s.Solution = append(s.Solution, SolutionItem{Iteration: iteration, Score: score, State: state})
+func addAdditionalInfo(s *Solution, itemName string, itemValue string) {
+	s.AdditionalInfo = append(s.AdditionalInfo, addtitionalInfor{ItemName: itemName, ItemValue: itemValue})
+}
+
+func setType(s *Solution, solutionType string) {
+	s.Type = solutionType
+}
+
+func (s *Solution) AddSolutionItem(iteration int, score int, state []int, deltaE ...int) {
+	defaultDeltaE := 0
+	if len(deltaE) > 0 {
+		defaultDeltaE = deltaE[0]
+	}
+	s.Solution = append(s.Solution, SolutionItem{Iteration: iteration, Score: score, State: state, DeltaE: defaultDeltaE})
 }
 
 func (s *Solution) GetSolution() []SolutionItem {
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"be/handlers"
+	h "be/handlers"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -12,17 +13,24 @@ func main() {
 	r := mux.NewRouter()
 
 	// Endpoints for algorithms
-	r.HandleFunc("/hill-climbing/steepest", handlers.HillClimbingSteepestHandler).Methods("POST")
-	r.HandleFunc("/hill-climbing/stochastic", handlers.HillClimbingStochasticHandler).Methods("POST")
-	r.HandleFunc("/hill-climbing/sidewaymove", handlers.HillClimbingSidewayMoveHandler).Methods("POST")
-	r.HandleFunc("/hill-climbing/randomrestart", handlers.HillClimbingRandomRestartHandler).Methods("POST")
-	r.HandleFunc("/genetic-algorithm", handlers.GeneticAlgorithmHandler).Methods("POST")
-	r.HandleFunc("/simulated-annealing", handlers.SimulatedAnnealingHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/steepest", h.HillClimbingSteepestHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/stochastic", h.HillClimbingStochasticHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/sidewaymove", h.HillClimbingSidewayMoveHandler).Methods("POST")
+	r.HandleFunc("/hill-climbing/randomrestart", h.HillClimbingRandomRestartHandler).Methods("POST")
+	r.HandleFunc("/genetic-algorithm", h.GeneticAlgorithmHandler).Methods("POST")
+	r.HandleFunc("/simulated-annealing", h.SimulatedAnnealingHandler).Methods("POST")
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to the Algorithms API!"))
 	})
 
+	// Allow CORS
+	corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
 	log.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", corsHandler(r)))
 }
