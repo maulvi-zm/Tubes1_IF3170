9dfc5def652eb938c41ab16c482152810f67a6e5
TuanOnta <mdz54dzakiarta@gmail.com>
2024-11-01 19:02:55 +0700

    feat: add stochastic algorithm

--- backend/algorithms/hill_climbing_stochastic.go
+++ backend/algorithms/hill_climbing_stochastic.go
@@ -1,6 +1,28 @@
 package algorithms
 
-func HillClimbingStochastic() string {
-    // Example pseudo-algorithm logic
-    return "Stochastic Hill Climbing completed."
+import "be/class"
+
+func HillClimbingStochastic(maxIter int) class.Solution {
+	currentCube := class.NewCube(5)
+	currentCube.SetRandomStartState()
+	currentScore := currentCube.GetCurrentScore()
+
+	res := class.NewSolution()
+	res.AddSolutionItem(0, currentScore, currentCube.GetCurrentState())
+
+	i := 1
+
+	for i+1 != maxIter {
+		randomSuccessor := currentCube.GetRandomSuccessor()
+		randomSuccessorScore := randomSuccessor.GetCurrentScore()
+
+		currentCube = randomSuccessor.CopyCube()
+		if currentScore < randomSuccessorScore {
+			currentScore = randomSuccessorScore
+			res.AddSolutionItem(i, currentScore, currentCube.GetCurrentState())
+		}
+		i++
+	}
+
+	return *res
 }
