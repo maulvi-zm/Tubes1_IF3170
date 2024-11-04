ee10dca8cd3b0973b8c4e5af1d768ccbf5ee6c2d
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-11-04 10:30:53 +0700

    feat: all information needed in simulated annealing

--- backend/algorithms/simulated_annealing.go
+++ backend/algorithms/simulated_annealing.go
@@ -4,13 +4,14 @@ import (
 	"be/class"
 	"fmt"
 	"math"
+	"time"
 )
 
 const (
 	T0                    = 100
 	alpha                 = 0.9
 	threshold             = 0.5
-	TEMPERATURE_THRESHOLD = 8
+	TEMPERATURE_THRESHOLD = 7
 )
 
 func getCurrentTemperature(k int) float64 {
@@ -32,12 +33,16 @@ func SimulatedAnnealing() class.Solution {
 	currentCube.SetRandomStartState()
 	currentScore := currentCube.GetCurrentScore()
 
+	timeStart := time.Now()
+
 	fmt.Println("Initial Score: ", currentScore)
 	res := class.NewSolution()
 	res.SetType("Simulated Annealing")
 	res.AddSolutionItem(0, currentScore, currentCube.GetCurrentState())
 
 	i := 1
+
+	stuck := 0
 
 	for {
 		successor := currentCube.GetRandomSuccessor()
@@ -52,23 +57,35 @@ func SimulatedAnnealing() class.Solution {
 		}
 
 		probability := getProbability(scoreDifference, currentTemperature)
-		if scoreDifference > 0 || (scoreDifference < 0 && isAcceptance(probability)) {
+		if scoreDifference > 0 {
 			currentCube = successor.CopyCube()
 			currentScore = successorScore
 
-			if i%1000 == 0 {
-				res.AddSolutionItem(i, currentScore, currentCube.GetCurrentState(), probability)
-				fmt.Println("Iteration: ", i, " Score: ", currentScore)
+		} else {
+			stuck++
+
+			if isAcceptance(probability) {
+				currentCube = successor.CopyCube()
+				currentScore = successorScore
 			}
-
-			i++
-
 		}
+
+		if i%10000 == 0 {
+			res.AddSolutionItem(i, currentScore, currentCube.GetCurrentState(), probability)
+			fmt.Println("Iteration: ", i, " Score: ", currentScore)
+		}
+
+		i++
 	}
 
-	if i%1000 != 0 {
+	if i%10000 != 0 {
 		res.AddSolutionItem(i, currentScore, currentCube.GetCurrentState())
 	}
+
+	elapsedTime := time.Since(timeStart).Milliseconds()
+	res.AddElapsedTime(float64(elapsedTime))
+	res.AddLastScore(currentScore)
+	res.AddAdditionalInfo("Stuck in Local Optimum Frequence", float64(stuck))
 
 	return *res
 }
